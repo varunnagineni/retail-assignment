4d4dbc471fd46ca03ce97d05fb80b9897ecd10f7
VarunNagineni <varunnagineni@nextgen.com>
2023-01-12 00:18:53 -0500

    1) Updated the query to accept id as Param

--- src/main/java/com/retail/rewards/repository/TransactionsRepository.java
+++ src/main/java/com/retail/rewards/repository/TransactionsRepository.java
@@ -4,6 +4,7 @@ import com.retail.rewards.model.CustomerRewardSummary;
 import com.retail.rewards.model.RewardTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,9 +19,9 @@ public interface TransactionsRepository extends JpaRepository<RewardTransaction,
      */
     @Query("SELECT r.customer.id as custId, MONTH(r.createdDate) as transMonth, sum(r.transAmount) as rewardsSummary " +
             "FROM RewardTransaction r " +
-            "WHERE r.customer.id = ?1 " +
+            "WHERE r.customer.id = :id " +
             "AND r.transStatus = 'APPROVED' " +
             "AND r.createdDate >= CURRENT_DATE - 90 " +
             "GROUP BY MONTH(r.createdDate)")
-    List<CustomerRewardSummary> getRewardSummaryById(Long id);
+    List<CustomerRewardSummary> getRewardSummaryById(@Param("id") Long id);
 }
